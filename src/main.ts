73e889c9553bdf157e299b1e7415be5dc11a23b9
VUVANTHAO <vuvanthao1603@gmail.com>
2026-01-14 23:41:57 +0700

    get user by id

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
   app.useStaticAssets(join(__dirname, '..', 'public')); //js, css, img
   app.setBaseViewsDir(join(__dirname, '..', 'views')); //views
   app.setViewEngine('ejs'); //su dung ejs thay vi hlb (mac dinh cua nestjs la hbs)
+
+  app.useGlobalPipes(new ValidationPipe()); //su dung class-validator o muc toan cuc
 
   await app.listen(String(configService.get<string>('PORT')));
 }
